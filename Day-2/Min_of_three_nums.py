1d80f25cb88b287b2a28dabe91fca26d441ad045
mugada-charan <mugadacharan07@gmail.com>
2025-09-22 18:35:01 +0530

    Day-2

---
+++ Day-2/Min_of_three_nums.py
@@ -0,0 +1,13 @@
+n1=int(input("Enter no: "))
+n2=int(input("Enter no: "))
+n3=int(input("Enter no: "))
+
+'''if n1<n2 and n1<n3:
+  print(f"The minimum number is {n1}")
+elif n2<n3:
+  print(f"The minimum number is {n2}")
+else:
+  print(f"The minimum number is {n3}")'''
+
+minimum=min(n1,n2,n3)
+print(f"The minimum number is {minimum}")
