9d7d32afe79bb4cd2d136d356eaadf505e3892ad
mugada-charan <mugadacharan07@gmail.com>
2025-09-23 20:11:11 +0530

    Day-3

---
+++ Day-3/even_odd.py
@@ -0,0 +1,10 @@
+n=int(input("Enter a number to check its even or odd:"))
+'''if n%2==0:
+  print('Even')
+else:
+  print('Odd')'''
+
+if n&1==1:
+  print('Odd')
+else:
+  print('Even')
---
+++ Day-3/pyramid.py
@@ -0,0 +1,11 @@
+'''
+    *
+   ***
+  *****
+ *******
+*********
+'''
+n=int(input("Enter: "))
+for i in range(1,n+1):
+  print(' '*(n-i),end='')
+  print('* '*i,end='\n')
